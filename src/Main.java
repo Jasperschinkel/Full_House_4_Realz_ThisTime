4ab6dbccc438d1bc2f16b40718befa08db6a51ce
daanbom <daan_bom@live.nl>
2019-06-05 17:15:54 +0200

    De nieuwe shit XD

--- src/Main.java
+++ src/Main.java
@@ -33,33 +33,7 @@ public class Main {
 
 
 
-    public static ArrayList<String> getLijst() throws Exception{
-        try{
-            Connection con = getConnection();
-            PreparedStatement state = con.prepareStatement("SELECT * FROM Spelers");
-            ResultSet result = state.executeQuery();
 
-            ArrayList<String> array = new ArrayList<String>();
-            while(result.next()){
-                System.out.print(result.getString("naam"));
-                System.out.print(result.getString("adres"));
-                System.out.println(result.getString("postcode"));
-                array.add(result.getString("naam"));
-            }
-            System.out.println("done");
-            return array;
-        } catch(Exception e){
-            System.out.println(e);}
-        return null;
-    }
 
-    public static void createTable() throws Exception{
-        try{
-            Connection con = getConnection();
-            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS tablename(id int NOT NULL)");
-            create.executeUpdate();
-        }catch(Exception e){
-            System.out.println(e);}
 
-    }
 }
