68c945d3db3f50f4f8b41dc4d09aac34446222e6
Hylius <orlandolandzaad@outlook.com>
2019-06-06 17:33:48 +0200

    Inschrijven lukt nu XD

--- src/Inschrijven.java
+++ src/Inschrijven.java
@@ -1,12 +1,8 @@
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 public class Inschrijven extends JFrame implements ActionListener {
     //Labels
