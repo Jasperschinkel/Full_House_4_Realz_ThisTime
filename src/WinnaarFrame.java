56af61bdfbf7b74b20d66be41bf0dc2a0848dfed
Gijs van der Giessen <gijsgiessen1@gmail.com>
2019-06-07 13:32:06 +0200

    Default Changelist

--- src/WinnaarFrame.java
+++ src/WinnaarFrame.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
-public class WinnaarFrame extends JFrame {
+public class WinnaarFrame extends JFrame implements ActionListener {
 
     private int aantalRondes;
 
@@ -29,11 +30,32 @@ public class WinnaarFrame extends JFrame {
         addActionListeners();
     }
 
-    public void setComponentBounds(){}
+    public void setComponentBounds(){
+        aantalRondesLabel.setBounds(50,50,100,50);
+        toernooiCodeLabel.setBounds(50,110,100,50);
+        aantalRondesField.setBounds(250,50,100,50);
+        toernooiCodeField.setBounds(250,110,100,50);
+        bevestigen.setBounds(215,340,100,50);
+        terug.setBounds(220,340,75,50);
+    }
 
-    public void addComponents(){}
+    public void addComponents(){
+        add(aantalRondesLabel);
+        add(aantalRondesField);
+        add(toernooiCodeLabel);
+        add(toernooiCodeField);
+        add(bevestigen);
+        add(terug);
+    }
 
-    public void addActionListeners(){}
+    public void addActionListeners(){
+        bevestigen.addActionListener(this);
+        terug.addActionListener(this);
+    }
+
+    public int getAantalRondes;
+
+    public int getToernooiCode;
 
 
 }
