6f999c6ff6aa3382840e55057cc25a72553cebd1
Gijs van der Giessen <gijsgiessen1@gmail.com>
2019-06-07 13:18:18 +0200

    Default Changelist

---
+++ src/WinnaarFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class WinnaarFrame extends JFrame {
+
+    JLabel aantalRondesLabel = new JLabel("Aantal rondes");
+    JTextField aantalRondesField = new JTextField();
+
+    JLabel toernooiCodeLabel = new JLabel("Toernooicode");
+    JTextField toernooiCodeField = new JTextField();
+
+    JButton bevestigen = new JButton("Klaar");
+    JButton terug = new JButton("terug");
+}
