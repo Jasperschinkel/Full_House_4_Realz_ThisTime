a6386cdbf0a74466b56d62a32c53036492a9993b
Gijs van der Giessen <gijsgiessen1@gmail.com>
2019-06-17 14:59:11 +0200

    Pls help

---
+++ Test/RegistratieWinnaarsTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+
+public class RegistratieWinnaarsTest {
+
+    @Test
+    public void getAantalTafelsFromDB() {
+        RegistratieWinnaars registratieWinnaars = new RegistratieWinnaars(2,3);
+
+        try{
+            int toernooiCode = registratieWinnaars.getToernooiCode();
+            Connection conn =
+                    DriverManager.getConnection("jdbc:mysql://meru.hhs.nl/18095240", "18095240",
+                            "Ene3shaise");
+            Statement stm;
+            stm = conn.createStatement();
+            String sql = "Select aantal_tafels From Toernooi WHERE TC =" +toernooiCode+";";
+            ResultSet rst;
+            rst = stm.executeQuery(sql);
+            if(rst.next()) {
+                int aantalTafels = rst.getInt("aantal_tafels");
+                registratieWinnaars.setAantelTafels(aantalTafels);
+
+            }
+        }catch (Exception e) {
+            System.out.println(e);}
+
+            assertEquals(14,registratieWinnaars.getAantaltafels());
+    }
+
+
+}
