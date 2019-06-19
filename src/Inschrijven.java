22aefbef70d02646370e3f7c35dd47fe62d88f03
Gijs van der Giessen <gijsgiessen1@gmail.com>
2019-06-19 18:58:34 +0200

    Default Changelist

--- src/Inschrijven.java
+++ src/Inschrijven.java
@@ -9,15 +9,13 @@ import java.sql.*;
 
 public class Inschrijven extends JFrame implements ActionListener {
     //Labels
-    private JLabel naamLabel = new JLabel("Spelercode: ");
-    private JLabel rankingLabel = new JLabel("Ranking: ");
+    private JLabel spelerIDLabel = new JLabel("Spelercode: ");
     private JLabel typeLabel = new JLabel("Type inschrijving: ");
     private JLabel codeLabel = new JLabel ("Code: ");
     private JLabel heeftBetaaldLabel = new JLabel("Heeft betaald: ");
 
     //Textfields
-    private JTextField naamField = new JTextField();
-    private JTextField rankingField = new JTextField();
+    private JTextField spelerIDField= new JTextField();
     private JTextField typeField = new JTextField();
     private JTextField codeField = new JTextField();
     private JTextField heeftBetaaldField = new JTextField();
@@ -25,7 +23,7 @@ public class Inschrijven extends JFrame implements ActionListener {
     //Buttons
     private JButton terugButton = new JButton("Terug");
     private JButton klaarButton = new JButton("Klaar");
-    private JButton rankingButton = new JButton("Krijg Ranking");
+
 
     public Inschrijven(){
         setTitle("Inschrijven");
@@ -41,56 +39,51 @@ public class Inschrijven extends JFrame implements ActionListener {
     }
 
     public void emptyTextField(){
-        naamField.setText("");
-        rankingField.setText("");
+        spelerIDField.setText("");
         typeField.setText("");
         codeField.setText("");
         heeftBetaaldField.setText("");
     }
 
     public void setComponentBounds(){
-        naamLabel.setBounds(40,10,100,40);
-        rankingLabel.setBounds(40,60,100,40);
+        spelerIDLabel.setBounds(40,10,100,40);
         typeLabel.setBounds(40,110,100,40);
         codeLabel.setBounds(40,160,200,40);
         heeftBetaaldLabel.setBounds(40, 210, 200, 40);
 
-        naamField.setBounds(250, 10, 100, 40);
-        rankingField.setBounds(250, 60, 100, 40);
+        spelerIDField.setBounds(250, 10, 100, 40);
         typeField.setBounds(250, 110, 100, 40);
         codeField.setBounds(250, 160, 100, 40);
         heeftBetaaldField.setBounds(250, 210, 100, 40);
 
         terugButton.setBounds(275,260,75,40);
         klaarButton.setBounds(175,260,100,40);
-        rankingButton.setBounds(50,260,125,40);
+
 
 
 
     }
 
     public void addComponents(){
-        add(naamLabel);
-        add(rankingLabel);
+        add(spelerIDLabel);
         add(typeLabel);
         add(codeLabel);
         add(heeftBetaaldLabel);
 
-        add(naamField);
-        add(rankingField);
+        add(spelerIDLabel);
         add(typeField);
         add(codeField);
         add(heeftBetaaldField);
 
         add(klaarButton);
         add(terugButton);
-        add(rankingButton);
+
     }
 
     public void addActionListeners(){
         terugButton.addActionListener(this);
         klaarButton.addActionListener(this);
-        rankingButton.addActionListener(this);
+
     }
 
     public void countSpelers() {
@@ -149,8 +142,8 @@ public class Inschrijven extends JFrame implements ActionListener {
             try {
                 Connection con = Main.getConnection();
                 PreparedStatement add = con.prepareStatement("INSERT INTO Inschrijvingen (naam, ranking, type_inschrijving, nummercode, heeft_betaald) VALUES (?,?,?,?,?);");
-                add.setString(1, naamField.getText());
-                add.setInt(2, Integer.parseInt(rankingField.getText()));
+                add.setString(1, spelerIDField.getText());
+                add.setInt(2, Integer.parseInt(spelerIDField.getText()));
                 add.setString(3, typeField.getText());
                 add.setInt(4, Integer.parseInt(codeField.getText()));
                 add.setString(5, heeftBetaaldField.getText());
@@ -166,7 +159,7 @@ public class Inschrijven extends JFrame implements ActionListener {
             try {
                 Connection con = Main.getConnection();
                 PreparedStatement st = con.prepareStatement("SELECT COUNT (*) as aantal FROM Inschrijvingen WHERE speler = ? AND toernooi = ?");
-                st.setInt(1, Integer.parseInt(naamField.getText()));
+                st.setInt(1, Integer.parseInt(spelerIDField.getText()));
                 st.setInt(2, Integer.parseInt(codeField.getText()));
                 ResultSet rs = st.executeQuery();
                 if (rs.next()) {
@@ -183,7 +176,7 @@ public class Inschrijven extends JFrame implements ActionListener {
             try{
                 Connection con = Main.getConnection();
                 PreparedStatement st = con.prepareStatement("SELECT COUNT (*) as aantal FROM Inschrijvingen WHERE speler = ? AND masterclass = ?");
-                st.setInt(1, Integer.parseInt(naamField.getText()));
+                st.setInt(1, Integer.parseInt(spelerIDField.getText()));
                 st.setInt(2, Integer.parseInt(codeField.getText()));
                 ResultSet rs = st.executeQuery();
                 if (rs.next()) {
@@ -204,7 +197,7 @@ public class Inschrijven extends JFrame implements ActionListener {
         try {
             Connection con = Main.getConnection();
             PreparedStatement st = con.prepareStatement("SELECT geslacht FROM Spelers WHERE idcode = ?");
-            st.setInt(1, Integer.parseInt(naamField.getText()));
+            st.setInt(1, Integer.parseInt(spelerIDField.getText()));
             ResultSet rs = st.executeQuery();
             if (rs.next()) {
                 String geslacht = rs.getString("geslacht");
@@ -253,6 +246,32 @@ public class Inschrijven extends JFrame implements ActionListener {
         else{return true;}
         }
 
+        public int getRanking() {
+            int spelerRanking = -1;
+
+            try {
+                Connection conn = Main.getConnection();
+                PreparedStatement ps = conn.prepareStatement("SELECT idcode, ranking FROM Spelers WHERE idcode = ?");
+                ps.setInt(1, Integer.parseInt(codeField.getText()));
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    int result = rs.getInt("idcode");
+                    if (result == Integer.parseInt(codeField.getText())) {
+                        PreparedStatement check = conn.prepareStatement("SELECT ranking FROM Spelers WHERE idcode = ?");
+                        check.setInt(1, Integer.parseInt(codeField.getText()));
+                        ResultSet resultaat = check.executeQuery();
+                        if (resultaat.next()) {
+                             spelerRanking = resultaat.getInt("ranking");
+                        }
+                    }
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+
+            }
+       return spelerRanking; }
+
 
     public boolean checkMaxRanking(){
         try {
@@ -267,8 +286,9 @@ public class Inschrijven extends JFrame implements ActionListener {
                     check.setInt(1, Integer.parseInt(codeField.getText()));
                     ResultSet resultaat = check.executeQuery();
                     if (resultaat.next()){
-                        if (resultaat.getInt("max_ranking") >= Integer.parseInt(rankingField.getText()))
+                        if (resultaat.getInt("max_ranking") >= getRanking()) {
                             return false;
+                        }
                     }
                 }
             }
@@ -279,27 +299,34 @@ public class Inschrijven extends JFrame implements ActionListener {
         return true;
     }
     public boolean checkInput(){
-        if(naamField.getText().equals("") || rankingField.getText().equals("") || typeField.getText().equals("") || codeField.getText().equals("") || heeftBetaaldField.getText().equals("")){
+        if(spelerIDField.getText().equals("") || typeField.getText().equals("") || codeField.getText().equals("") || heeftBetaaldField.getText().equals("")){
             return true;
         } else { return false; }
         }
 
-    public int getRanking(){
-        String naam = naamField.getText();
-        int ranking=0;
-        try {
-            Connection con = Main.getConnection();
-            PreparedStatement st = con.prepareStatement("SELECT ranking FROM Spelers WHERE idcode = ?");
-            st.setInt(1, Integer.parseInt(naam));
-            ResultSet rs= st.executeQuery();
-            if(rs.next()) {
-                return rs.getInt("ranking");
-            }
-        }catch(Exception e) {
-            System.out.println(e);
-        }
-        return ranking;
-    }
+
+
+
+//    public int getRanking(){
+//        String naam = naamField.getText();
+//        int ranking=0;
+//        try {
+//            Connection con = Main.getConnection();
+//            PreparedStatement st = con.prepareStatement("SELECT ranking FROM Spelers WHERE idcode = ?");
+//            st.setInt(1, Integer.parseInt(naam));
+//            ResultSet rs= st.executeQuery();
+//            if(rs.next()) {
+//                return rs.getInt("ranking");
+//            }
+//        }catch(Exception e) {
+//            System.out.println(e);
+//        }
+//        return ranking;
+//    }
+
+
+
+
 
     public int getMaxAantalInschrijvingen(){
         if(typeField.getText().equals("Toernooi")) {
@@ -399,9 +426,7 @@ public class Inschrijven extends JFrame implements ActionListener {
                 emptyTextField();
             }
         }
-        if(e.getSource() == rankingButton){
-            rankingField.setText(Integer.toString(getRanking()));
-        }
+
 
     }
 }
